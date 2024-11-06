94453f11e6b7213b0c318b59e73caaf77383bbfc
era <newtoolsworks@gmail.com>
2024-11-06 00:02:46 -0600

    fix service

--- core/service/linklayer_auth.py
+++ core/service/linklayer_auth.py
@@ -10,28 +10,24 @@ class Authenticator:
         self.conn:duckdb.DuckDBPyConnection = conn
 
     def handle_connn(self,sockClient:socket.socket):
-        try:
-            reader = sockClient.makefile()
-            username = reader.readline()
-            password = reader.readline()
-            username = username.strip()
-            password = password.strip()
-            print("username "+username+" "+password)
-            exists =self.conn.execute("select expdisable,expire from users where username=? and password=?",(username,password)).fetchall()
-            if len(exists) == 0:
+        reader = sockClient.makefile()
+        username = reader.readline()
+        password = reader.readline()
+        username = username.strip()
+        password = password.strip()
+        
+        exists =self.conn.execute("select expdisable,expire from users where username=? and password=?",(username,password)).fetchall()
+        if len(exists) == 0:
+            sockClient.sendall(bytes([0]))
+        else:
+            expdisable = exists[0][0]
+            expire = exists[0][1]
+            now = int(time.time())
+            if not expdisable and now > expire:
                 sockClient.sendall(bytes([0]))
             else:
-                expdisable = exists[0][0]
-                expire = exists[0][1]
-                now = int(time.time())
-                if not expdisable and now > expire:
-                    sockClient.sendall(bytes([0]))
-                else:
-                    sockClient.sendall(bytes([1]))
-            sockClient.close()
-            
-        except Exception as e:
-            print(e)
+                sockClient.sendall(bytes([1]))
+        sockClient.close()
 
     def start_server(self):
         if os.path.exists(self.path):
