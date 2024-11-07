4dadfb7710e09b3b36ad4af886c89bf08ceaaee1
era <newtoolsworks@gmail.com>
2024-11-06 18:58:12 -0600

    fix service

--- core/service/linklayer.py
+++ core/service/linklayer.py
@@ -20,7 +20,7 @@ class LinkLayer:
         self.status = Linklayer_status.OFF
         self.process:subprocess.Popen = None
 
-    def discard(io:IO):
+    def discard(self,io:IO):
         while True:
             algo = io.readline().decode("utf-8").strip()
             if algo == "":
