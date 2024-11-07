dc457782ac001e351594099bb903f723cdb6149f
era <newtoolsworks@gmail.com>
2024-11-06 20:21:39 -0600

    test

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import jsons
-from entity.linklayer import HTTP, HTTPTLS, TLS, Config, Service
+from entity.linklayer import HTTP, HTTPTLS, TLS, TLS1, Config, HTTPResponse, Service
 
 
 cfg = Config()
 service = Service()
 httptls = HTTPTLS()
-http = HTTP()
-http.Listen = "0.0.0.0:80"
+http = HTTPResponse()
+
 http.Response = "HTTP/1.1 200 OK\r\n\r\n"
-tls = TLS()
+tls = TLS1
 tls.Key = "/opt/fladsf"
 tls.Cert = "/opt/flafd"
-tls.Listen = "0.0.0.0:443"
+
 
 httptls.Http = http
 httptls.TLS = tls
-
+httptls.Listen = "0.0.0.0:440"
 service.type = "httptls"
 service.cfg = httptls
 
