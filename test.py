3d30435cae93301e4062fc97754f419595e1f595
era <newtoolsworks@gmail.com>
2024-11-06 20:19:45 -0600

    fix service

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 import jsons
-from entity.linklayer import Config
+from entity.linklayer import HTTP, HTTPTLS, TLS, Config, Service
 
 
 cfg = Config()
+service = Service()
+httptls = HTTPTLS()
+http = HTTP()
+http.Listen = "0.0.0.0:80"
+http.Response = "HTTP/1.1 200 OK\r\n\r\n"
+tls = TLS()
+tls.Key = "/opt/fladsf"
+tls.Cert = "/opt/flafd"
+tls.Listen = "0.0.0.0:443"
+
+httptls.Http = http
+httptls.TLS = tls
+
+service.type = "httptls"
+service.cfg = httptls
+
+cfg.services.append(service)
 print(jsons.dumps(cfg.__dict__))
