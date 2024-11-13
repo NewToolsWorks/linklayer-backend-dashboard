ddcf80f05d7427247c12995f8947cc44ff19346f
era <newtoolsworks@gmail.com>
2024-11-12 22:44:16 -0600

    login

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def session_handle(request: Request, call_next):
             response = await call_next(request)
             return response
         except Exception as ex:
-            print("exception")
+            print("exception "+ex)
             return RedirectResponse("/login")
     if path.startswith("/login"):
         
